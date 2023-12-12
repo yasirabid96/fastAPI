4f99cba31d8c9ecf3c65cfb0827ee1f7379e4bd7
Muhammad Yasir <yasirabid96@gmail.com>
2023-12-12 14:45:31 +1000

    FASTAPI PROJECT

---
+++ main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+from fastapi.params import Body
+
+app = FastAPI()
+
+# request Get method URL: "/"
+
+
+@app.get("/")
+def read_root():
+
+    return {"Hello": "World bro"}
+
+
+@app.get("/posts")
+def get_posts():
+    return {"data": "your first post"}
+
+
+@app.post("/createpost")
+def create_post(payload: dict=Body(...)):
+    print(payload)
+    return {"message": "sucessfully created post"}
